d005a65a0564b79c76f6108d8b6450ba7cab56ec
Ayeza <ayezabashir442@gmail.com>
2023-07-10 22:08:38 +0500

    sign in functionality

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if(array_key_exists('logout', $_GET)){
   unset($_SESSION);
   setcookie('id', '', time() - 60*60);
   $_COOKIE['id'] = '';
-} else if(array_key_exists('id',$_SESSION) OR array_key_exists('id',$_COOKIE)){
+} else if((array_key_exists('id',$_SESSION) AND $_SESSION['id'] ) AND array_key_exists('id',$_COOKIE) AND $_COOKIE['id']){
   header("loggedIn.php");
 }
 
@@ -26,37 +26,60 @@ if(array_key_exists('submit', $_POST)){
     $error .= "Password is required!";
   }
   if($error!=""){
-    $error = "<p>There were error(s) in your form</p>";
+    $error .= "<p>There were error(s) in your form</p>";
   }
   else{
-    $query = "SELECT id FROM `users` WHERE email='".mysqli_real_escape_string($link,$_POST['email'])."' LIMIT 1 ";
-    $result = mysqli_query($link, $query);
+    if(isset($_POST['signUp'])){
 
-    if(mysqli_num_rows($result)>0){
-      $error = "Email address is already taken";
-    }
-    else{
-      $query = "INSERT INTO `users` (`email`,`password`) VALUES ('".mysqli_real_escape_string($link,$_POST['email'])."','".mysqli_real_escape_string($link,$_POST['password'])."')";
-      
-      if(!mysqli_query($link, $query)){
-        $error = "<p>Could not Sign up! Please try again.</p>";
-      }
-      else{
-        $query = "UPDATE `users` SET password = 
-                  '".md5(mysqli_insert_id($link).$_POST['password'])."'
-                  WHERE id= '".mysqli_insert_id($link)."'
-                  LIMIT 1
-                ";
+        $query = "SELECT id FROM `users` WHERE email='".mysqli_real_escape_string($link,$_POST['email'])."' LIMIT 1 ";
+        $result = mysqli_query($link, $query);
 
-        mysqli_query($link, $query);
-
-        $_SESSION['id'] = mysqli_insert_id($link);
-        if($_POST['stayLoggedIn'] = '1'){
-          setcookie('id', mysqli_insert_id($link), time()+ 60*60*24*365);
+        if(mysqli_num_rows($result)>0){
+        $error = "Email address is already taken";
         }
+        else{
 
-        header('Location: loggedIn.php');
-      }
+            $query = "INSERT INTO `users` (`email`,`password`) VALUES ('".mysqli_real_escape_string($link,$_POST['email'])."','".mysqli_real_escape_string($link,$_POST['password'])."')"; 
+            if(!mysqli_query($link, $query)){
+            $error = "<p>Could not Sign up! Please try again.</p>";
+            }
+            else{
+                $query = "UPDATE `users` SET password = 
+                            '".md5(mysqli_insert_id($link).$_POST['password'])."'
+                            WHERE id= '".mysqli_insert_id($link)."'
+                            LIMIT 1
+                        ";
+
+                mysqli_query($link, $query);
+
+                $_SESSION['id'] = mysqli_insert_id($link);
+                if($_POST['stayLoggedIn'] == '1'){
+                setcookie('id', mysqli_insert_id($link), time()+ 60*60*24*365);
+                }
+
+                header('Location: loggedIn.php');
+            }
+        }
+    }else{
+        $query = "SELECT * FROM `users` WHERE email='".mysqli_real_escape_string($link, $_POST['email'])."' ";
+        $result = mysqli_query($link, $query);
+        $row = mysqli_fetch_array($result);
+        if(isset($row)){
+          $hashedPassword = md5(md5($row['id']).$_POST['password']);
+          if($hashedPassword = $row['password']){
+            $_SESSION['id'] = $row['id'];
+            if($_POST['stayLoggedIn'] = '1'){
+              setcookie('id', mysqli_insert_id($link), time()+ 60*60*24*365);
+            }
+            header('Location: loggedIn.php');
+          }
+          else{
+            $error = 'email/password combination could not be found';
+          }
+        }
+        else{
+          $error = 'email/password combination could not be found';
+        }
     }
   }
 }
@@ -64,9 +87,20 @@ if(array_key_exists('submit', $_POST)){
 ?>
 
 <div id=''> <?php echo $error ?></div>
+<!-- SIGN UP -->
 <form method="post">
   <input type="email" name="email" placeholder="Email" id="" />
   <input type="password" name="password" placeholder="Password" />
   <input type="checkbox" name="stayLoggedIn" value="1" id="" />
+  <input type="hidden" name="signUp" value='1'>
   <input type="submit" name="submit" value="Sign Up" />
+</form>
+
+<!-- SIGN IN -->
+<form method="post">
+  <input type="email" name="email" placeholder="Email" id="" />
+  <input type="password" name="password" placeholder="Password" />
+  <input type="checkbox" name="stayLoggedIn" value="1" id="" />
+  <input type="hidden" name="logIn" value='0'>
+  <input type="submit" name="submit" value="Log In" />
 </form>
